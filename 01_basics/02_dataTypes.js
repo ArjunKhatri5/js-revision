4f69867ddee033ccc059e166bcf7a452d2aa239a
ArjunKhatri5 <99785617+ArjunKhatri5@users.noreply.github.com>
2023-11-01 09:57:47 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version.
+
+//alert(3 +3) we are using node JS not browser, hence it will show error.
+
+let name = "arjun";
+let age = 21;
+let isLoggedIn = true;
+
+// number => 2 to power 53
+//bigInt 
+// string => ""
+// boolean => true/false
+//null => stand alone value
+// undefined => no values defined yet
+// symbol => unique
+
+// object
+
+console.log(typeof "Arjun");
+
+//in JavaScript, the data type of null is an object
+console.log("the type of null is: ", typeof null);           // shows "object"
+console.log("the type of undefined is: ", typeof undefined);      // shows "undefined"
