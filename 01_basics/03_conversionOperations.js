9126c600712a103ae38a064b27331fade5e6e93c
ArjunKhatri5 <99785617+ArjunKhatri5@users.noreply.github.com>
2023-11-08 06:51:28 +0000

    conversion

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -24,5 +24,46 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let somenumber = 33;
 
 let somenumberToString = String(somenumber);
-console.log(somenumberToString);
-console.log(typeof somenumberToString);
+// console.log(somenumberToString);
+// console.log(typeof somenumberToString);
+
+
+// **********Operations*****************
+
+let value = 100;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello ";
+let str2 = "Arjun";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);               // prints 12
+// console.log(1 + "2");               // prints 12
+// console.log("1" + "2");             // prints 12
+// console.log("1" + 2 + 2);           // prints 122
+// console.log(1 + 2 + "2");           // prints 32
+// console.log(1 + (2 + "2"));           // prints 122
+
+
+// console.log(true);      // prints true
+// console.log(+true);      // prints 1
+// console.log(true+);      // shows error
+
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);           // prints 101
+
+console.log(gameCounter++);         // still prints 101 but increments the value for after print (for next line)
+console.log(gameCounter);           // prints 102
+
+console.log(++gameCounter);         // prints 103, since increment first, prints later
