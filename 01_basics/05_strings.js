f621c5586fa21876e1baf0c5140d76a38540a20e
ArjunKhatri5 <99785617+ArjunKhatri5@users.noreply.github.com>
2023-11-11 09:41:43 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,28 @@
+const name = "Arjun";
+let age = 21;
+let fastFood = "Burger";
+
+// console.log(`My name is ${name}, I am ${age} years old. I like ${fastFood}.`);
+
+
+
+
+const nameOfCharacter = "My name is Naruto Uzumaki";
+
+let newName = nameOfCharacter.replace("Naruto", "Minato");
+// console.log(nameOfCharacter);
+// console.log(newName);
+
+
+
+let fName = "   Arjun   ";
+// console.log(fName);
+let noWhiteSpaceName = fName.trim();
+// console.log(noWhiteSpaceName);
+
+
+
+let anything = "Hello, Arjun here, how're you";
+// console.log(anything.includes("Arjun"));     //true
+// console.log(anything.includes("Khatri"));        // false
+
